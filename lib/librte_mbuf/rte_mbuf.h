01bbe78e3f2ad486a5dc889068ca7dd8a268baca
Paul Emmerich <emmericp@net.in.tum.de>
2015-05-17 00:03:08 +0200

    add tx crc disable flag
    (cherry picked from commit 9f843660de35d4902e822e47cbebe5d093315751)
    
     Conflicts:
    	lib/librte_mbuf/rte_mbuf.c
    	lib/librte_mbuf/rte_mbuf.h

--- lib/librte_mbuf/rte_mbuf.h
+++ lib/librte_mbuf/rte_mbuf.h
@@ -371,10 +371,16 @@ extern "C" {
 #define PKT_TX_OUTER_IPV6    (1ULL << 60)
 
 /**
+ * Disable CRC checksum offload
+ */
+#define PKT_TX_NO_CRC_CSUM   (1ULL << 61)
+
+/**
  * Bitmask of all supported packet Tx offload features flags,
  * which can be set for packet.
  */
 #define PKT_TX_OFFLOAD_MASK (    \
+		PKT_TX_NO_CRC_CSUM |	 \
 		PKT_TX_OUTER_IPV6 |	 \
 		PKT_TX_OUTER_IPV4 |	 \
 		PKT_TX_OUTER_IP_CKSUM |  \
